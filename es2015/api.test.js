48d11244d52d7ada656e5a6a8c0f6a061e729a3c
Ángel González <aglezabad@gmail.com>
2017-08-02 09:12:33 +0200

    Trying to get Travis build pass (#11)

--- es2015/api.test.js
+++ es2015/api.test.js
@@ -2,4 +2,16 @@
 
 // Test our plugin using DocPad's testers
 var path = require('path');
-require('docpad').require('testers').test({ testerClass: 'RendererTester', pluginPath: path.join(__dirname, '..') });
+require('docpad').require('testers').test({
+    testerName: 'api plugin common test',
+    testerClass: 'ServerTester',
+    pluginPath: path.join(__dirname, '..'),
+    pluginName: 'api',
+    autoExit: 'safe'
+}, {
+    plugins: {
+        api: {
+            cfgSrc: ['testapi1/dpaconfig.json']
+        }
+    }
+});
