5d7d5d792ee5c7ed7c12eb1bfb0cf50c27cbad6d
Ángel González <aglezabad@gmail.com>
2017-08-02 10:25:20 +0200

    I#13 to master (#14)
    * Working on #11
    
    * Working on #11
    
    * Trying to get Travis build pass (#11)
    
    * Downgrading joe module due to bevry/joe#19
    
    * Rechecking #11

--- src/api.test.js
+++ src/api.test.js
@@ -1,3 +1,17 @@
 // Test our plugin using DocPad's testers
 const path = require('path');
-require('docpad').require('testers').test({testerClass: 'RendererTester', pluginPath: path.join(__dirname, '..')});
+require('docpad').require('testers').test({
+    testerName: 'api plugin common test',
+    testerClass: 'ServerTester',
+    pluginPath: path.join(__dirname, '..'),
+    pluginName: 'api',
+    autoExit: 'safe'
+}, {
+    plugins: {
+        api: {
+            cfgSrc: [
+                'testapi1/dpaconfig.json'
+            ]
+        }
+    }
+});
